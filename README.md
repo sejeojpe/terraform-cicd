5e42e039cd30831a58d228678272e6f3463b8785
jesus <sejeojepe@gmail.com>
2025-04-14 20:29:13 +0200

    Tarea 2: CI/CD con Terraform y LocalStack

---
+++ README.md
@@ -0,0 +1,2 @@
+# Terraform CI/CD con LocalStack
+![Deploy Terraform](https://github.com/sejeojpe/terraform-cicd/actions/workflows/deploy.yml/badge.svg)
---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  s3_use_path_style = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "mi_bucket" {
+  bucket = "mi-bucket-cicd"
+  force_destroy = true
+  tags = {
+    Proyecto = "CI/CD con LocalStack"
+  }
+}
