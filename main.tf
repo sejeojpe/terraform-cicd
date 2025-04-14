5f4898f820f71f72371b9769dbc8a87e6c5c3e95
jesus <sejeojepe@gmail.com>
2025-04-14 20:42:28 +0200

    Tarea 2: CI/CD con Terraform y LocalStack

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
   secret_key = "test"
   skip_credentials_validation = true
   skip_metadata_api_check = true
+  skip_requesting_account_id = true
   s3_use_path_style = true
   endpoints {
     s3 = "http://localhost:4566"
